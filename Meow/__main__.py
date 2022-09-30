807bd427ec01215b230fb1ffa39347182db051a3
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-09-30 08:09:55 +0530

    Update __main__.py

--- Meow/__main__.py
+++ Meow/__main__.py
@@ -2,7 +2,7 @@ from pyrogram import idle
 from pyrogram import Client as Bot
 from Meow.clientbot import run
 from Meow.config import API_ID, API_HASH, BOT_TOKEN
-
+from . import meow, hl
     
 bot = Bot(
     ":memory:",
@@ -11,6 +11,9 @@ bot = Bot(
     bot_token=BOT_TOKEN,
     plugins=dict(root="plugins")
 )
+
+if meow:
+    meow.start()
 
 bot.start()
 run()
