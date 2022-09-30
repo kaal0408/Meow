750194d20b44728092594c72b4ac30495c8d56dc
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-09-30 09:57:30 +0530

    Update helpers.py

--- Meow/core/helpers.py
+++ Meow/core/helpers.py
@@ -1,4 +1,4 @@
 
 
-from Main.config_var import Config
-from Main.helper_func.basic_helpers import edit_or_reply, is_admin_or_owner
+from Meow import config
+from Meow.helper_func.basic_helpers import edit_or_reply, is_admin_or_owner
