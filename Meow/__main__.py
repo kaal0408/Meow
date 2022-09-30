4c78a09fdb4ec96f40e00f78fee4d5f5696f9a8c
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-09-30 08:04:22 +0530

    Update __main__.py

--- Meow/__main__.py
+++ Meow/__main__.py
@@ -1,7 +1,7 @@
 from pyrogram import idle
 from pyrogram import Client as Bot
-from modules.clientbot import run
-from modules.config import API_ID, API_HASH, BOT_TOKEN
+from Meow.clientbot import run
+from Meow.config import API_ID, API_HASH, BOT_TOKEN
 
     
 bot = Bot(
