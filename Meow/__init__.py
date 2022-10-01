2861e28afbfc32fae2de464c17f954e074729f73
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-01 13:01:30 +0530

    Update __init__.py

--- Meow/__init__.py
+++ Meow/__init__.py
@@ -45,7 +45,7 @@ CMD_HELP = {}
 # SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
 #----------------------------------------------
 
-app = Client(name="SESSION", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="Spam.Modules"))
+app = Client(name="SESSION", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="Meow.Modules"))
 print("Client 1 Found")
 
 
