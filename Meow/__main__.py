c0b49f12ae2d8fdfbacd23ed634748f1682098bd
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-10-02 15:27:30 +0530

    Update __main__.py

--- Meow/__main__.py
+++ Meow/__main__.py
@@ -1,28 +1,13 @@
 # KAAL X - Telegram Projects
 # (c) 2022 - 2023 KAAL
 # Don't Kang 
+# Contribution by @Alone_loverboy
 
 
+from Meow.client import callMe
 from pytgcalls import idle
-import asyncio
-from pyrogram import idle
-from . import (app, hl, arq, call_py)
 
-
-async def main():
-    await call_py.start()
-    print(
-        """
-    ------------------
-   | ƲՏЄƦƁƠƬ ƛƇƬƖƔЄƊ! |
-    ------------------
-"""
-    )
-    await idle()
-    await arq.close()
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-
-
+if __name__ == __main__:
+    callMe.start()
+    idle()
+    print("Meow Deployed Successfully✅")
