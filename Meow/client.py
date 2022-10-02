0e7aff335a881b70ccd74611fd51b398897ece03
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-02 19:38:19 +0530

    Update client.py

--- Meow/client.py
+++ Meow/client.py
@@ -8,7 +8,7 @@ from pytgcalls import PyTgCalls
 from . import (SESSION, API_ID, API_HASH)
 
 
-app = Client(name="SESSION", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="Meow.Modules"))
+app = Client(name="Meow", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="Meow.Modules"))
 print("Client  Found")
 
 
