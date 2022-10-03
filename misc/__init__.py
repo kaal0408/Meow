2b10d2dde9e6e63b43483e17bd45003bb99131ee
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-03 20:09:56 +0530

    Update __init__.py

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,10 +1,10 @@
 from misc.helpers import edit_or_reply, get_text
-from misc.decorators import dynamic
+from misc.decorators import app
 
 __all__ = (
     "edit_or_reply",
     "get_text",
-    "dynamic",
+    "app",
     "edit_or_send_as_file",
     "get_user",
     "is_admin_or_owner"
