b7905261023e7f21d45c8e5245a4003a96b0fb5b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-03 19:45:38 +0530

    Add files via upload

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,19 @@
+import os
+import logging
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from startup.config import MONGO_DB
 
+
+mongodb = MongoClient(MONGO_DB)
+
+
+try:
+    mongodb.server_info()
+except ConnectionFailure:
+    logging.error("Invalid Mongo DB URL. Please Check Your Credentials! Astro2.0 is Exiting!")
+    quit(1)
+
+
+
+dtbs = mongodb["Astro"]
