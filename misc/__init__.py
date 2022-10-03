62927e70d0b3615c3147801c055a60d3d45a3e26
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-03 23:47:25 +0530

    Update __init__.py

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,10 +1,10 @@
 from misc.helpers import edit_or_reply, get_text
-from misc.decorators import app
+from misc.decorators import manjeet
 
 __all__ = (
     "edit_or_reply",
     "get_text",
-    "app",
+    "manjeet",
     "edit_or_send_as_file",
     "get_user",
     "is_admin_or_owner"
