7168031758057532a8baa04b10c91491efc6da9e
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-04 11:40:58 +0530

    Update afk.py

--- Meow/Modules/afk.py
+++ Meow/Modules/afk.py
@@ -12,7 +12,7 @@ from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client, filters
 
 from . import *
-from misc import edit_or_reply, manjeet, get_text
+from misc import edit_or_reply, manjeet, get_text, Logme
 from database.afkdb import (
     no_afk,
     go_afk,
