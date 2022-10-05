178800013737a34bb0434a940c5be79b4c7758ee
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-06 02:13:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+#
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Config:
+    def __init__(self) -> None:      
+        self.SPOTIFY: bool = False
+        self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
+        self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
+        self.STREAM_MODE: str = (
+            "audio"
+            if (os.environ.get("STREAM_MODE", "audio").lower() == "audio")
+            else "video"
+        )
+        self.ADMINS_ONLY: bool = os.environ.get("ADMINS_ONLY", False)
+        self.SPOTIFY_CLIENT_ID: str = os.environ.get("SPOTIFY_CLIENT_ID", None)
+        self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
+
+
+config = Config()
