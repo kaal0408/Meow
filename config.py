722e30544e88489c0676e623fa42eda845df08aa
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-06 02:15:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-#
 
 import os
 from dotenv import load_dotenv
@@ -20,6 +19,53 @@ class Config:
         self.ADMINS_ONLY: bool = os.environ.get("ADMINS_ONLY", False)
         self.SPOTIFY_CLIENT_ID: str = os.environ.get("SPOTIFY_CLIENT_ID", None)
         self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
+
+import asyncio
+import os
+import sys
+import time
+
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+__version__ = "v0.1"
+
+# -------------CONFIGS--------------------
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+ALIVE_MSG = os.getenv("ALIVE_MSG", "")
+PING_MSG = os.getenv("PING_MSG", "")
+SESSION = os.getenv("SESSION", None)
+TIMEZONE = os.environ.get("TIMEZONE", "")
+LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
+MONGO_DB = os.environ.get("MONGO_DB", "")
+    
+HNDLR = os.getenv("HNDLR", ".")
+sudo = os.getenv("SUDO_USERS")
+
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
+
+
+
+
+
+# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
+#----------------------------------------------
+
+
+
+hl = HNDLR[0]
+start_time = time.time()
+
+#-------------------------CLIENTS-----------------------------
+
 
 
 config = Config()
