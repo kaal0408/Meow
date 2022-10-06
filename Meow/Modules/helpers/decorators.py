dd5164781067c0feb4dbe3efa5ca54d49599a328
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-07 00:40:12 +0530

    Update decorators.py

--- Meow/Modules/helpers/decorators.py
+++ Meow/Modules/helpers/decorators.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 
 from config import SUDO_USERS
-from MusicAndVideo.helpers.admins import get_administrators
+from Meow.Modules.helpers.admins import get_administrators
 
 
 def authorized_users_only(func: Callable) -> Callable:
