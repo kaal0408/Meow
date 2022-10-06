ae23d5731a5b6bf5c38e1ecc15b92c36f3d16e5f
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-07 00:31:00 +0530

    Update user_id.py

--- Meow/Modules/user_id.py
+++ Meow/Modules/user_id.py
@@ -4,7 +4,9 @@ from pyrogram.types import Message
 from Meow.Modules.helpers.get_file_id import get_file_id
 from Meow import HNDLR, LOGS_CHANNEL, SUDO_USERS, app
 
-@Client.on_message(filters.command(["id"], prefixes=f"{HNDLR}"))
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["id"], prefixes=HNDLR))
+@Client.on_message(filters.me & filters.command(["id"], prefixes=HNDLR))
 async def showid(_, message: Message):
     chat_type = message.chat.type
 
