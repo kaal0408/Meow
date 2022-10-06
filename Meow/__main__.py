e34856e3534435db076aa3b41fe41f4436798fba
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-07 00:13:11 +0530

    Update __main__.py

--- Meow/__main__.py
+++ Meow/__main__.py
@@ -4,11 +4,15 @@
 # Contribution by @Alone_loverboy
 from pyrogram import idle
 import asyncio
-from . import (app as Manjeet, hl, TIMEZONE, LOGS_CHANNEL, MONGO_DB, SUDO_USERS  )
+from . import (app as Manjeet, hl, TIMEZONE, LOGS_CHANNEL, MONGO_DB, SUDO_USERS, call_py  )
+from pytgcalls import PyTgCalls
+from pytgcalls import idle as pyidle
 
 if Manjeet:
     Manjeet.start()
     idle()
+    call_py.start()
+    pyidle()
     print("Meow Deployed Successfully✅")
 
 
