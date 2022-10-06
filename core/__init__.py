9d7d329d7db6cb07e5e9846d568d289b7e366d8c
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-06 13:41:48 +0530

    Delete core directory

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-from core.song import Song
-from core.admins import is_sudo, is_admin
-from core.stream import app, ydl, sweetie, pytgcalls, skip_stream, start_stream
-from core.groups import (
-    get_group, get_queue, set_group, set_title, all_groups, clear_queue,
-    set_default, shuffle_queue)
-from core.funcs import (
-    search, check_yt_url, extract_args, generate_cover, delete_messages,
-    get_spotify_playlist, get_youtube_playlist)
