163e9bfe70ef837bc1ecdaea2936d3203e11012b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-10 22:15:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
         self.API_HASH: str = os.environ.get("API_HASH", None)
         self.SESSION: str = os.environ.get("SESSION", None)
         self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
-        self.SUDOERS: list = [
+        self.SUDO_USERS: list = [
             int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
@@ -24,13 +24,53 @@ class Config:
         self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
         self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
         self.STREAM_MODE: str = (
-            "audio"
-            if (os.environ.get("STREAM_MODE", "audio").lower() == "audio")
-            else "video"
+            "video"
+            if (os.environ.get("STREAM_MODE", "video").lower() == "video")
+            else "audio"
         )
         self.ADMINS_ONLY: bool = os.environ.get("ADMINS_ONLY", False)
         self.SPOTIFY_CLIENT_ID: str = os.environ.get("SPOTIFY_CLIENT_ID", None)
         self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
+
+import asyncio
+import os
+import sys
+import time
+
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+__version__ = "v0.1"
+
+# -------------CONFIGS--------------------
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+ALIVE_MSG = os.getenv("ALIVE_MSG", "")
+PING_MSG = os.getenv("PING_MSG", "")
+SESSION = os.getenv("SESSION", None)
+TIMEZONE = os.environ.get("TIMEZONE", "")
+LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
+MONGO_DB = os.environ.get("MONGO_DB", "")
+    
+HNDLR = os.getenv("HNDLR", ".")
+sudo = os.getenv("SUDO_USERS")
+
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
+
+
+
+
+# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
+#----------------------------------------------
+
 
 
 config = Config()
