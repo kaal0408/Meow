d7fdf071af156c0c23445f6597d09c8dfd11ff0b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-11 23:46:39 +0530

    Update __main__.py

--- Meow/__main__.py
+++ Meow/__main__.py
@@ -4,13 +4,25 @@
 # Contribution by @Alone_loverboy
 from pyrogram import idle
 import asyncio
-from . import (app, hl, TIMEZONE, LOGS_CHANNEL, MONGO_DB, SUDO_USERS )
+from . import (app, app2, app3, app4, app5, hl, TIMEZONE, LOGS_CHANNEL, MONGO_DB, SUDO_USERS )
 
-app.start()
+if app:
+    app.start()
+print("Your 1Client Successfully Deployed ✅")
+if app2:
+    app2.start()
+print("Your 2Client Successfully Deployed ✅")
+if app3:
+    app3.start()
+print("Your 3Client Successfully Deployed ✅")
+if app4:
+    app4.start()
+print("Your 4Client Successfully Deployed ✅")
+if app5:
+    app5.start()
+print("Your 5Client Successfully Deployed ✅")
+
 idle()
-print("UserBot Started")
-
-
 
 
 
