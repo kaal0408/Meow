075147056032a453abe42c8ad86125624cc81dbb
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-12 12:52:36 +0530

    Update __init__.py

--- Meow/__init__.py
+++ Meow/__init__.py
@@ -44,9 +44,6 @@ if sudo:
 
 # SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
 #----------------------------------------------
-
-
-
 app = Client(
     name="[Meow]",
     session_string=SESSION,
@@ -57,53 +54,33 @@ app = Client(
 )
 print("Client 1 Found")
 
-app2 = Client(
-    name="[Meow]",
-    session_string=SESSION2,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    in_memory=True,
-    plugins={'root': 'Meow.Modules'}
-)
-print("Client 2 Found")
+
+
+if SESSION2:
+    app2 = Client(name="SESSION2", api_id = API_ID, api_hash = API_HASH, session_string=SESSION2, plugins=dict(root="Meow.Modules"))
+    print("Client 2 Found")
 else:
     app2 = None
 
-app3 = Client(
-    name="[Meow]",
-    session_string=SESSION3,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    in_memory=True,
-    plugins={'root': 'Meow.Modules'}
-)
-print("Client 3 Found")
+if SESSION3:
+    app3 = Client(name="SESSION3", api_id = API_ID, api_hash = API_HASH, session_string=SESSION3, plugins=dict(root="Meow.Modules"))
+    print("Client 3 Found")
 else:
     app3 = None
 
-app4 = Client(
-    name="[Meow]",
-    session_string=SESSION4,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    in_memory=True,
-    plugins={'root': 'Meow.Modules'}
-)
-print("Client 4 Found")
+if SESSION4:
+    app4 = Client(name="SESSION4", api_id = API_ID, api_hash = API_HASH, session_string=SESSION4, plugins=dict(root="Meow.Modules"))
+    print("Client 4 Found")
 else:
     app4 = None
 
-app5 = Client(
-    name="[Meow]",
-    session_string=SESSION5,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    in_memory=True,
-    plugins={'root': 'Meow.Modules'}
-)
-print("Client 5 Found")
+if SESSION5:
+    app5 = Client(name="SESSION5", api_id = API_ID, api_hash = API_HASH, session_string=SESSION5, plugins=dict(root="Meow.Modules"))
+    print("Client 5 Found")
 else:
     app5 = None
+
+
 
 
 
