1734bb939ccf7acd1b8c5cfea98b1ee1d791cf39
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-12 22:45:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,69 +1,52 @@
+import asyncio
 import os
-from os import getenv
+import sys
+import time
 
 from dotenv import load_dotenv
+from pyrogram import Client, filters
 
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
-que = {}
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-API_ID = int(getenv("API_ID", "6435225"))
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-LOG_CHAT = int(getenv("LOG_CHAT", "777000"))
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "")
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "")
-SPOTIFY_USERNAME = getenv("SPOTIFY_USERNAME", "")
-MONGO_DB = getenv("MONGO_DB", "")
-ALIVE_IMG = getenv("ALIVE_IMG", "")
-DB_URL = getenv("DATABASE_URL", "")
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
-STRING_SESSION2 = getenv("STRING_SESSION2", "")
-STRING_SESSION3 = getenv("STRING_SESSION3", "")
-STRING_SESSION4 = getenv("STRING_SESSION4", "")
-STRING_SESSION5 = getenv("STRING_SESSION5", "")
-STRING_SESSION6 = getenv("STRING_SESSION6", "")
-STRING_SESSION7 = getenv("STRING_SESSION7", "")
-STRING_SESSION8 = getenv("STRING_SESSION8", "")
-STRING_SESSION9 = getenv("STRING_SESSION9", "")
-STRING_SESSION10 = getenv("STRING_SESSION10", "")
-STRING_SESSION11 = getenv("STRING_SESSION11", "")
-STRING_SESSION12 = getenv("STRING_SESSION12", "")
-STRING_SESSION13 = getenv("STRING_SESSION13", "")
-STRING_SESSION14 = getenv("STRING_SESSION14", "")
-STRING_SESSION15 = getenv("STRING_SESSION15", "")
-STRING_SESSION16 = getenv("STRING_SESSION16", "")
-STRING_SESSION17 = getenv("STRING_SESSION17", "")
-STRING_SESSION18 = getenv("STRING_SESSION18", "")
-STRING_SESSION19 = getenv("STRING_SESSION19", "")
-STRING_SESSION20 = getenv("STRING_SESSION20", "")
-STRING_SESSION21 = getenv("STRING_SESSION21", "")
-STRING_SESSION22 = getenv("STRING_SESSION22", "")
-STRING_SESSION23 = getenv("STRING_SESSION23", "")
-STRING_SESSION24 = getenv("STRING_SESSION24", "")
-STRING_SESSION25 = getenv("STRING_SESSION25", "")
-STRING_SESSION26 = getenv("STRING_SESSION26", "")
-STRING_SESSION27 = getenv("STRING_SESSION27", "")
-STRING_SESSION28 = getenv("STRING_SESSION28", "")
-STRING_SESSION29 = getenv("STRING_SESSION29", "")
-STRING_SESSION30 = getenv("STRING_SESSION30", "")
-STRING_SESSION31 = getenv("STRING_SESSION31", "")
-STRING_SESSION32 = getenv("STRING_SESSION32", "")
-STRING_SESSION33 = getenv("STRING_SESSION33", "")
-STRING_SESSION34 = getenv("STRING_SESSION34", "")
-STRING_SESSION35 = getenv("STRING_SESSION35", "")
-STRING_SESSION36 = getenv("STRING_SESSION36", "")
-STRING_SESSION37 = getenv("STRING_SESSION37", "")
-STRING_SESSION38 = getenv("STRING_SESSION38", "")
-STRING_SESSION39 = getenv("STRING_SESSION39", "")
-STRING_SESSION40 = getenv("STRING_SESSION40", "")
-STRING_SESSION41 = getenv("STRING_SESSION41", "")
-STRING_SESSION42 = getenv("STRING_SESSION42", "")
-STRING_SESSION43 = getenv("STRING_SESSION43", "")
-STRING_SESSION44 = getenv("STRING_SESSION44", "")
-STRING_SESSION45 = getenv("STRING_SESSION45", "")
-STRING_SESSION46 = getenv("STRING_SESSION46", "")
-STRING_SESSION47 = getenv("STRING_SESSION47", "")
-STRING_SESSION48 = getenv("STRING_SESSION48", "")
-STRING_SESSION49 = getenv("STRING_SESSION49", "")
-STRING_SESSION50 = getenv("STRING_SESSION50", "")
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+__version__ = "v0.1"
+
+# -------------CONFIGS--------------------
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+ALIVE_MSG = os.getenv("ALIVE_MSG", "")
+PING_MSG = os.getenv("PING_MSG", "")
+SESSION = os.getenv("SESSION", None)
+SESSION2 = os.getenv("SESSION2", None)
+SESSION3 = os.getenv("SESSION3", None)
+SESSION4 = os.getenv("SESSION4", None)
+SESSION5 = os.getenv("SESSION5", None)
+TIMEZONE = os.environ.get("TIMEZONE", "Asia/Kolkata")
+LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
+MONGO_DB = os.environ.get("MONGO_DB", "")
+ DB_URL = getenv("DATABASE_URL", "")   
+HNDLR = os.getenv("HNDLR", ".")
+sudo = os.getenv("SUDO_USERS")
+
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
+
+
+
+
+# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
+#----------------------------------------------
+
+
+
+
+
+
+hl = HNDLR[0]
+start_time = time.time()
+
 
