d23731c48ffcdfd62b2bbdae551a10df1de35c3c
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 22:07:46 +0530

    Update Broadcast.py

--- Meow/Modules/Broadcast.py
+++ Meow/Modules/Broadcast.py
@@ -1,6 +1,6 @@
 
 
-from Meow import (app, app2, app3, app4, app5, HNDLR, SUDO_USERS, LOGS_CHANNEL )
+from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client , filters
 import asyncio
 from pyrogram.types import Message
