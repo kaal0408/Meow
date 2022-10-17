1f21a5f36cc278723b5dde634e09d54cb1b16721
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 22:19:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,77 @@ from dotenv import load_dotenv
 if os.path.exists("local.env"):
     load_dotenv("local.env")
 que = {}
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-API_ID = int(getenv("API_ID", "6435225"))
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-LOG_CHAT = int(getenv("LOG_CHAT", "777000"))
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "")
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "")
-SPOTIFY_USERNAME = getenv("SPOTIFY_USERNAME", "")
-MONGO_DB = getenv("MONGO_DB", "")
-ALIVE_IMG = getenv("ALIVE_IMG", "")
-DB_URL = getenv("DATABASE_URL", "")
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+
+
+
+import asyncio
+import os
+import sys
+import time
+
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+__version__ = "v0.1"
+
+# -------------CONFIGS--------------------
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+ALIVE_MSG = os.getenv("ALIVE_MSG", "")
+PING_MSG = os.getenv("PING_MSG", "")
+SESSION = os.getenv("SESSION", None)
+TIMEZONE = os.environ.get("TIMEZONE", "Asia/Kolkata")
+LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
+MONGO_DB = os.environ.get("MONGO_DB", "")
+DB_URL = os.getenv("DATABASE_URL", "")
+   
+HNDLR = os.getenv("HNDLR", ".")
+sudo = os.getenv("SUDO_USERS")
+
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
+
+contact_filter = filters.create(
+    lambda _, __, message: (message.from_user and message.from_user.is_contact)
+    or message.outgoing
+)
+
+
+# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
+#----------------------------------------------
+
+
+if SESSION:
+   app = Client(
+    name="[Meow]",
+    session_string=SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    in_memory=True,
+    plugins={'root': 'Meow.Modules'}
+)
+   print("Client 1 Found")
+else: 
+   app = none
+
+call_py = PyTgCalls(app)
+
+
+
+
+
+
+hl = HNDLR[0]
+start_time = time.time()
+
+#-------------------------CLIENTS-----------------------------
+
+
+
