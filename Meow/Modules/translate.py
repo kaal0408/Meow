fd7b5bad39aa0b92f0f10f622107e746cdefa7e4
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 21:53:15 +0530

    Update translate.py

--- Meow/Modules/translate.py
+++ Meow/Modules/translate.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 from py_trans import Async_PyTranslator
 from helpers.pyrohelper import get_arg
-from Meow import (app, app2, app3, app4, app5, HNDLR, SUDO_USERS, LOGS_CHANNEL )
+from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client, filters
 
 
