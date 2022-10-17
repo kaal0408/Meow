740ff5bc448a429a2f652544ce61e615b71abe2b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 21:51:47 +0530

    Update whois.py

--- Meow/Modules/whois.py
+++ Meow/Modules/whois.py
@@ -9,7 +9,7 @@ from pyrogram.errors import PeerIdInvalid
 
 
 from helpers.PyroHelpers import ReplyCheck
-from Meow import (app, app2, app3, app4, app5, HNDLR, SUDO_USERS, LOGS_CHANNEL )
+from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client, filters
 
 
