f925f5443a141df4c1c3a4a2dd20d82ddb289924
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 21:52:32 +0530

    Update unmute.py

--- Meow/Modules/unmute.py
+++ Meow/Modules/unmute.py
@@ -12,7 +12,7 @@ unmute_permissions = ChatPermissions(
     can_pin_messages=False,
 )
 
-from Meow import (app, app2, app3, app4, app5, HNDLR, SUDO_USERS, LOGS_CHANNEL )
+from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client, filters
 
 
