671ff4fff32e350e6dc30ea9e6139c54dc46c5e5
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 21:57:08 +0530

    Update promote.py

--- Meow/Modules/promote.py
+++ Meow/Modules/promote.py
@@ -11,7 +11,7 @@ from pyrogram.errors import (
     UserAdminInvalid,
     FloodWait,
 )
-from Meow import (app, app2, app3, app4, app5, HNDLR, SUDO_USERS, LOGS_CHANNEL )
+from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client, filters
 
 
