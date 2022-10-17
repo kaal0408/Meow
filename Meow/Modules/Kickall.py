a3a15b97680d44a6a01221ba89eeaad5c4ff7415
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-17 22:06:02 +0530

    Update Kickall.py

--- Meow/Modules/Kickall.py
+++ Meow/Modules/Kickall.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Meow import (app, app2, app3, app4, app5, HNDLR, SUDO_USERS, LOGS_CHANNEL )
+from Meow import (app, HNDLR, SUDO_USERS, LOGS_CHANNEL )
 from pyrogram import Client, filters
 
 
