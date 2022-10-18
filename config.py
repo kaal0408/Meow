cf82fecb8419eeb3ed69b3ed59a2c8ddcdfd65cd
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-18 22:01:37 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,82 +1,29 @@
 import os
-from os import getenv
-
-from dotenv import load_dotenv
-
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
-que = {}
-
-
-
-import asyncio
-import os
-import sys
-import time
 
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
 
-
+# For Local Deploy
 if os.path.exists(".env"):
     load_dotenv(".env")
-    
-__version__ = "v0.1"
 
-# -------------CONFIGS--------------------
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-ALIVE_PIC = os.getenv("ALIVE_PIC", "")
-ALIVE_MSG = os.getenv("ALIVE_MSG", "")
-PING_MSG = os.getenv("PING_MSG", "")
-SESSION = os.getenv("SESSION", None)
-TIMEZONE = os.environ.get("TIMEZONE", "Asia/Kolkata")
-LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
-MONGO_DB = os.environ.get("MONGO_DB", "")
-DB_URL = os.getenv("DATABASE_URL", "")
-   
-HNDLR = os.getenv("HNDLR", ".")
-sudo = os.getenv("SUDO_USERS")
+# Necessary Vars
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
+HNDLR = os.getenv("HNDLR", "!")
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
-SUDO_USERS = []
-if sudo:
-    SUDO_USERS = make_int(sudo)
 
 contact_filter = filters.create(
     lambda _, __, message: (message.from_user and message.from_user.is_contact)
     or message.outgoing
 )
 
-
-# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
-#----------------------------------------------
-
-
-if SESSION:
-   app = Client(
-    name="[Meow]",
-    session_string=SESSION,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    in_memory=True,
-    plugins={'root': 'Meow.Modules'}
-)
-   print("Client 1 Found")
-else: 
-   app = none
-
-call_py = PyTgCalls(app)
-
-
-
-
-
-
-hl = HNDLR[0]
-start_time = time.time()
-
-#-------------------------CLIENTS-----------------------------
-
-
-
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicRioUserbot"))
+call_py = PyTgCalls(bot)
+# © 2022 GitHub, Inc.
+# Terms
+# Privacy
+# Security
