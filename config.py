01723d88766f359f0f62740742e1129a02f04ce2
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-18 22:11:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
+import asyncio
+import sys
+import time
 
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
 
 # For Local Deploy
 if os.path.exists(".env"):
@@ -12,18 +20,31 @@ if os.path.exists(".env"):
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "!")
+HNDLR = os.getenv("HNDLR", ".")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
+ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+ALIVE_MSG = os.getenv("ALIVE_MSG", "")
+PING_MSG = os.getenv("PING_MSG", "")
 
+sudo = os.getenv("SUDO_USERS")
+
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
 
 contact_filter = filters.create(
     lambda _, __, message: (message.from_user and message.from_user.is_contact)
     or message.outgoing
 )
-
+# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
+#----------------------------------------------
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicRioUserbot"))
 call_py = PyTgCalls(bot)
-# © 2022 GitHub, Inc.
+
 # Terms
 # Privacy
 # Security
+
+hl = HNDLR[0]
+start_time = time.time()
+
